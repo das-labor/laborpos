8376f29226afbb8c83e0f69c58d40dbb6873724e
Endres <git@e7p.de>
2018-11-10 13:51:27 +0100

    new product database, new bon printer but not fully working

--- buy.php
+++ buy.php
@@ -7,9 +7,24 @@ $printstr = "";
 foreach ($boughts as $bought) {
 	$product = html_entity_decode($bought->product);
 	fwrite($f, $date . "\t" . $ip . "\t" . $bought->price . "\t" . $product . "\n");
-	$printstr .= substr(date('d.H:i') . " " . $bought->price . " " . $product, 0, 24) . "\r\n";
+	
+	$datet = strtotime($date);
+	$price = str_pad(number_format(floatval($bought->price), 2, ',', ''), 6, ' ', STR_PAD_LEFT);
+	$printstr .= substr((date('d.m. H:i', $datet) . " " . $price . " €  " . $product), 0, 44) . "\r\n";
 }
 fclose($f);
-exec("echo \"".rtrim($printstr)."\\r\" | lpr -P bondrucker");
+//exec("echo \"".rtrim($printstr)."\\r\" | lpr -P bondrucker");
+
+$descriptorspec = array(
+   0 => array("pipe", "r"),
+   1 => array("pipe", "w")
+);
+$process = proc_open("python3 /home/pi/bonprint.py", $descriptorspec, $pipes);
+if (is_resource($process)) {
+    fwrite($pipes[0], utf8_decode($printstr));
+    fclose($pipes[0]);
+    fclose($pipes[1]);
+    proc_close($process);
+}
 echo "OK";
 ?>
--- tools/printlog.php
+++ tools/printlog.php
@@ -1,9 +1,31 @@
 <?php
 $boughts = file("../db/boughts");
-foreach ($boughts as $bought) {
-	list($datestr, $ip, $pricetext, $product) = explode("\t", trim($bought));
-	$date = strtotime($datestr);
-	$price = floatval($pricetext);
-	echo substr((date('d.H:i', $date) . " " . $price . " " . $product), 0, 24) . "\r\n";
+$from = 277;
+
+$descriptorspec = array(
+   0 => array("pipe", "r"),
+   1 => array("pipe", "w"),
+   2 => array("pipe", "w")
+);
+$process = proc_open("python3 /home/pi/bonprint.py", $descriptorspec, $pipes);
+if (is_resource($process)) {
+	foreach ($boughts as $bought) {
+		if ($from > 1)
+		{
+			--$from;
+			continue;
+		}
+		list($datestr, $ip, $pricetext, $product) = explode("\t", trim($bought));
+		$date = strtotime($datestr);
+		$price = str_pad(number_format(floatval($pricetext), 2, ',', ''), 6, ' ', STR_PAD_LEFT);
+		$printstr = substr((date('d.m. H:i', $date) . " " . $price . " €  " . $product), 0, 44) . "\r\n";
+		echo $printstr;
+		fwrite($pipes[0], $printstr);
+	}
+	fclose($pipes[0]);
+	echo fread($pipes[1],1000);
+	echo fread($pipes[2],1000);
+	fclose($pipes[1]);
+	proc_close($process);
 }
 ?>
